527ec9391909b7c424e66a07c2423438d3e00e08
John Vandenberg <jayvdb@gmail.com>
2016-04-20 20:48:49 +0700

    Update ruby tests

--- spec/install_spec.rb
+++ spec/install_spec.rb
@@ -1,26 +1,38 @@
 
 describe 'install.sh' do
-  it 'mounts two gigabytes of shared memory at /dev/shm' do
-    directory = '/dev/shm'
+  context 'when container mode is not active', :if => ENV.has_key?('ORACLE_HOME') && ENV['ORACLE_HOME'].start_with?('/u01') do
+    it 'mounts two gigabytes of shared memory at /dev/shm' do
+      directory = '/dev/shm'
 
-    expect(File).to exist(directory)
-    expect(File.lstat(directory)).to be_directory
-    expect(`df -B1 #{directory}`.split("\n")[1].split(/\b/)[2].to_i).to be >= 2147483648
+      expect(File).to exist(directory)
+      expect(File.lstat(directory)).to be_directory
+      expect(`df -B1 #{directory}`.split("\n")[1].split(/\b/)[2].to_i).to be >= 2147483648
+    end
+
+    it 'creates an executable that does nothing at /sbin/chkconfig' do
+      executable = '/sbin/chkconfig'
+
+      expect(File).to exist(executable)
+      expect(File.lstat(executable).mode).to eq('100744'.to_i(8))
+      expect(IO.read(executable)).to eq("#!/bin/sh\n")
+    end
+
+    it 'creates a directory at /var/lock/subsys' do
+      directory = '/var/lock/subsys'
+
+      expect(File).to exist(directory)
+      expect(File.lstat(directory)).to be_directory
+    end
+
   end
 
-  it 'creates an executable that does nothing at /sbin/chkconfig' do
-    executable = '/sbin/chkconfig'
+  context 'when container mode is not active and ORACLE_FILE is defined', :if => ENV.has_key?('ORACLE_HOME') && ENV['ORACLE_HOME'].start_with?('/u01') && ENV.has_key?('ORACLE_FILE') do
+    it 'creates /etc/init.d/oracle-xe' do
+      executable = '/etc/init.d/oracle-xe'
 
-    expect(File).to exist(executable)
-    expect(File.lstat(executable).mode).to eq('100744'.to_i(8))
-    expect(IO.read(executable)).to eq("#!/bin/sh\n")
-  end
-
-  it 'creates a directory at /var/lock/subsys' do
-    directory = '/var/lock/subsys'
-
-    expect(File).to exist(directory)
-    expect(File.lstat(directory)).to be_directory
+      expect(File).to exist(executable)
+      expect(IO.read(executable)).to eq("#!/bin/bash\n")
+    end
   end
 
   context 'when ORACLE_FILE is defined', :if => ENV.has_key?('ORACLE_FILE') do
@@ -29,8 +41,6 @@ describe 'install.sh' do
     end
 
     it 'installs Oracle Express Edition' do
-      expect(File).to exist('/etc/init.d/oracle-xe')
-
       expect(File).to exist(ENV['ORACLE_HOME'])
       expect(File).to be_executable(ENV['ORACLE_HOME'] + '/bin/sqlplus')
     end
