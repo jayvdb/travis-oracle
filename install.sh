100e6ea7874d4c6658d43a04becead9d1ab503a3
John Vandenberg <jayvdb@gmail.com>
2016-05-08 16:09:59 +0700

    Install real chkconfig
    chkconfig is a whitelisted package, so it shouldnt be
    destroyed by travis-oracle.

--- install.sh
+++ install.sh
@@ -7,14 +7,21 @@ ORACLE_RPM="$(basename $ORACLE_FILE .zip)"
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-dpkg -s bc libaio1 rpm unzip > /dev/null 2>&1 ||
-  ( sudo apt-get -qq update && sudo apt-get --no-install-recommends -qq install bc libaio1 rpm unzip )
+DEPS="bc libaio1 rpm unzip"
+if [ "$_system_version" = "12.04" ]; then
+   DEPS="$DEPS chkconfig"
+fi
+dpkg -s $DEPS > /dev/null 2>&1 ||
+  ( sudo apt-get -qq update && sudo apt-get --no-install-recommends -qq install $DEPS )
+
+# chkconfig expects /sbin/insserv to exist
+sudo ln -s /usr/lib/insserv/insserv /sbin/insserv
 
 df -B1 /dev/shm | awk 'END { if ($1 != "shmfs" && $1 != "tmpfs" || $2 < 2147483648) exit 1 }' ||
   ( sudo rm -r /dev/shm && sudo mkdir /dev/shm && sudo mount -t tmpfs shmfs -o size=2G /dev/shm )
 
 test -f /sbin/chkconfig ||
-  ( echo '#!/bin/sh' | sudo tee /sbin/chkconfig > /dev/null && sudo chmod u+x /sbin/chkconfig )
+  ( echo '#!/bin/sh' | sudo tee /sbin/chkconfig > /dev/null && sudo chmod 755 /sbin/chkconfig )
 
 test -d /var/lock/subsys || sudo mkdir /var/lock/subsys
 
